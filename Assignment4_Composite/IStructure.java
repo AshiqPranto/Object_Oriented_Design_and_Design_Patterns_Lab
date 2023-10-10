748fe1e037f565a69de06e6dfdcd6890ad770522
AshiqPranto <ashiqpranto@gmail.com>
2023-10-10 21:35:57 +0600

    assignment 4 done

--- Assignment4_Composite/IStructure.java
+++ Assignment4_Composite/IStructure.java
@@ -1,5 +1,8 @@
 package Assignment4_Composite;
 
 public interface IStructure {
-    
+    public void enter();
+    public void exit();
+    public void location();
+    public String getName();
 }
